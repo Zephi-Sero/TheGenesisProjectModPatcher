36ac4df2f46bc4fb15135fb55bc31179c7a17201
Zephi Sero <repsi012@gmail.com>
2021-09-02 08:58:16 -0500

    v0.2.0 Changes - new dependency system & Modus support

---
+++ TheGenesisProjectModPatcher/Chat/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternalPatcher.Chat;
+
+namespace TheGenesisProjectModPatcher.Chat {
+    /// <summary>
+    /// Extendable chat command
+    /// </summary>
+    public abstract class ChatCommand : ModdedChatCommand {
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        /// <param name="cmd">Command name</param>
+        public ChatCommand(string cmd) : base(cmd) {}
+    }
+}
---
+++ TheGenesisProjectModPatcher/Chat/CommandAlias.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternalPatcher.Chat;
+
+namespace TheGenesisProjectModPatcher.Chat {
+    /// <summary>
+    /// Allows you to register aliases for commands
+    /// </summary>
+    public static class CommandAlias {
+        /// <summary>
+        /// Registar an alias
+        /// </summary>
+        /// <param name="alias">Command that, when typed, will resolve to aliasOf</param>
+        /// <param name="aliasOf">Name of command that will be run</param>
+        public static void RegisterAlias(string alias, string aliasOf) {
+            ModdedCommands.commandaliases.Add(alias, aliasOf);
+        }
+    }
+}
